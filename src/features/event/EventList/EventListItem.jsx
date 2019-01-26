8c08881751b0a10f7bcdd2af8a7a401cee5cfd4c
Harley Salas <harley@harleysalas.com>
2019-01-26 11:54:31 -0500

    End of section 4

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -14,7 +14,7 @@ class EventListItem extends Component {
                <Item.Content>
                  <Item.Header as="a">{event.title}</Item.Header>
                  <Item.Description>
-                   Hosted by <a>{event.hostedby}</a>
+                   Hosted by <a>{event.hostedBy}</a>
                  </Item.Description>
                </Item.Content>
              </Item>
@@ -28,7 +28,7 @@ class EventListItem extends Component {
          </Segment>
          <Segment secondary>
            <List horizontal>
-             {event.attendees.map((attendee) => (
+             {event.attendee && event.attendees.map((attendee) => (
                <EventListAttendee attendee={attendee} key={attendee.id}/>
              ))}
            </List>
